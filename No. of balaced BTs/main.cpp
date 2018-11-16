bd50f532904e6d725349e00d64686ee7419a4694
chitresh97 <chitu1997@gmail.com>
2018-11-16 23:21:23 +0530

    no. of balanced BTs(Memoization)

--- No. of balaced BTs/main.cpp
+++ No. of balaced BTs/main.cpp
@@ -1,5 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
+int binaryTreesOfHeightHHelper(int h,int *ans){
+  if(h <= 1) {
+		return 1;
+	}
+  if(ans[h-1]!=-1)
+    return ans[h-1];
+
+	int mod = (int) (pow(10, 9)) + 7;
+	int x = binaryTreesOfHeightHHelper(h - 1,ans);
+	int y = binaryTreesOfHeightHHelper(h - 2,ans);
+
+	int temp1 = (int)(((long)(x)*x) % mod);
+	int temp2 = (int)((2* (long)(x) * y) % mod);
+	int an = (temp1 + temp2) % mod;
+
+
+	//int ans = (x*x + 2*x*y) % mod;
+  ans[h-1]=an;
+	return an;
+}
+
+//Using memoization
+int binaryTreesOfHeightH(int h) {
+    // Write your code here
+  int *ans=new int[h];
+  for(int i=0;i<h;i++){
+    ans[i]=-1;
+  }
+  binaryTreesOfHeightHHelper(h,ans);
+
+}
+
+
 //Using brute force
 int balancedBTs(int h) {
   if(h <= 1) {
